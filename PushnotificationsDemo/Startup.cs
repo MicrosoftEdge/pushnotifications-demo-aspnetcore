25e25b5a65819bb948e56df8f6cb7408f3c93a0f
Mark Szabo <maorka@live.com>
2018-09-25 16:48:05 +0200

    Startup config

--- PushnotificationsDemo/Startup.cs
+++ PushnotificationsDemo/Startup.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Net.Http.Headers;
 
 namespace PushnotificationsDemo
 {
@@ -24,14 +19,6 @@ namespace PushnotificationsDemo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -49,8 +36,32 @@ namespace PushnotificationsDemo
             }
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseCookiePolicy();
+
+            app.Use(async (context, next) =>
+            {
+                context.Response.Headers.Add("X-Frame-Options", new[] { "SAMEORIGIN" });
+                context.Response.Headers.Add("Expect-CT", new[] { "expect-ct: max-age=604800, report-uri=https://example.com" });
+                context.Response.Headers.Add("X-XSS-Protection", new[] { "1; mode=block; report=https://example.com" });
+                context.Response.Headers.Add("X-Content-Type-Options", new[] { "nosniff" });
+                context.Response.Headers.Add("Referrer-Policy", new[] { "strict-origin-when-cross-origin" });
+                context.Response.Headers.Add("Feature-Policy", new[] { "accelerometer 'none'; camera 'none'; geolocation 'self'; gyroscope 'none'; magnetometer 'none'; microphone 'none'; payment 'none'; usb 'none'" });
+                context.Response.Headers.Add("Content-Security-Policy", new[] { "default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self' data:; connect-src https: wss: 'self'; font-src 'self'; frame-src 'self'; form-action 'self'; upgrade-insecure-requests; report-uri https://example.com" });
+                context.Response.Headers.Remove(HeaderNames.Server);
+                context.Response.Headers.Remove("X-Powered-By");
+                await next();
+            });
+
+            app.UseResponseCompression();
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                OnPrepareResponse = ctx =>
+                {
+                    const int cacheExpirationInSeconds = 60 * 60 * 24 * 30; //one month
+                    ctx.Context.Response.Headers[HeaderNames.CacheControl] =
+                        "public,max-age=" + cacheExpirationInSeconds;
+                }
+            });
 
             app.UseMvc(routes =>
             {
