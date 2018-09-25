e7d96299f04da91d575c5d2c80809c61684dc777
Mark Szabo <maorka@live.com>
2018-09-25 16:27:01 +0200

    Add project files.

---
+++ PushnotificationsDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PushnotificationsDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
